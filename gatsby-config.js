05dfd148f07a95cb01b3e9d74dc371783402c657
Frankie Rodriguez <rfrankie04@gmail.com>
2019-08-03 23:15:18 -0400

    Prep for katrice

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Mad Beats Philly`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@elemental_5`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
